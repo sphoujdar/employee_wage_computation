3c7a8984781224a6f5842d74cc0c4cf6f8f6b001
Shubham <sm.phoujdar@gmail.com>
2020-11-17 06:12:48 +0530

    Modified : Changed parameters such that part time employee is demonstrated. All required code was already present from design time.

--- employeeWageComputation/runnerWageComputation.java
+++ employeeWageComputation/runnerWageComputation.java
@@ -8,10 +8,10 @@ public class runnerWageComputation {
 		Company firstCompany = new Company();
 		
 		firstCompany.addEmployee(1, true);
-		firstCompany.addEmployee(2, true);
-		firstCompany.addEmployee(3, true);
-		//firstCompany.addEmployee(4, true);
-		//firstCompany.addEmployee(5, true);
+		//firstCompany.addEmployee(2, true);
+		firstCompany.addEmployee(3, false);
+		firstCompany.addEmployee(4, true);
+		firstCompany.addEmployee(5, false);
 		//firstCompany.addEmployee(6, true);
 		
 		System.out.println("Monthly wage for the added employees is : " + firstCompany.calculateMonthlyWage());
